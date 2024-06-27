59d53323f67ec7a42df4fb4d340bd5d3c49cdfce
rameshtudu <rameshsonbad@gmail.com>
2024-06-28 00:09:31 +0530

    Create simple_calculator.cpp
    Task-2-To create a simple calculator using cpp.

---
+++ simple_calculator.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+usiung namespace stdl
+int main(){
+char op;
+float num1, num2;
+
+cout << "Enter operator: +, -, *, /: ";
+cin >> op;
+
+cout <<" Enter two operands: ";
+cin >> num1 >> num2;
+
+switch(op) {
+
+    case '+':
+        cout << num1 << " + " << num2 << " = " << num1 + num2;
+        break;
+
+    case '-':
+        cout << num1 << " - " << num2 << " = " << num1 - num2;
+        break;
+
+    case '*':
+        cout << num1 << " * " << num2 << " = " << num1 * num2;
+        break;
+
+    case '/':
+        cout << num1 << " / " << num2 << " = " << num1 / num2;
+        break;
+
+    default:
+        cout << "Error! operator is not correct";
+        break;
+}
+
+return 0;
+}
